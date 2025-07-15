4b5508634550987a733a73727c7993c08960a88b
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-07-15 14:28:02 +1200

    got three js CDN on

--- static/main.js
+++ static/main.js
@@ -1,3 +1,11 @@
-<script>
-  
-</script>
+var script = document.createElement("script");
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"; // three js CDN
+document.appendChild(script);
+
+script.onload = function() {
+    beginScene();
+}
+
+function beginScene() {
+    
+}
