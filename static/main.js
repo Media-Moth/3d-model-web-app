b7f7805dd9312fe0587b0634f2120a78f603c9aa
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-07-23 14:21:53 +1200

    added three js code

--- static/main.js
+++ static/main.js
@@ -1,11 +1,40 @@
-var script = document.createElement("script");
-script.src = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"; // three js CDN
-document.appendChild(script);
 
-script.onload = function() {
-    beginScene();
-}
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-function beginScene() {
-    
+
+var url = window.location;
+url = url.pathname.slice(7)
+console.log(url); // prints model id
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls( camera, renderer.domElement );
+
+renderer.setSize( window.innerWidth / 1.5, window.innerHeight / 1.5);
+renderer.setAnimationLoop( animate );
+
+document.getElementById("3dcanvas").appendChild( renderer.domElement );
+renderer.domElement.className = "centreCanvas";
+const geometry = new THREE.TorusKnotGeometry( 1, 0.4, 100, 16 ); 
+const material = new THREE.MeshPhongMaterial({color: 0xff00ff, flatShading: true});
+const cube = new THREE.Mesh( geometry, material );
+const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+
+scene.add(light);
+scene.add(cube);
+
+
+camera.position.z = 5;
+
+function animate() {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.02;
+    renderer.render( scene, camera );
+
 }
+window.addEventListener('resize', function() {
+    renderer.setSize( window.innerWidth / 1.5, window.innerHeight / 1.5);
+});
