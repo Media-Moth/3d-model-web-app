175983d5a4f80e1e824188c0d0a1c0e5281e47ed
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-07-29 14:38:36 +1200

    can preview uploaded images before

--- static/modelUpload.js
+++ static/modelUpload.js
@@ -6,7 +6,6 @@ var url = window.location;
 url = url.pathname.slice(7)
 console.log(url); // prints model id
 
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -16,6 +15,34 @@ const controls = new OrbitControls( camera, renderer.domElement );
 renderer.setSize( window.innerWidth / 1.5, window.innerHeight / 1.5);
 renderer.setAnimationLoop( animate );
 
+const input = document.getElementById("input");
+input.addEventListener("change", updateModelView);
+
+function updateModelView()
+{
+    const file = input.files[0];
+
+    scene.clear();
+    const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+    scene.add(light);
+    
+
+
+    const loader = new GLTFLoader();
+    loader.load(URL.createObjectURL(file), (gltf) => {
+    const material = new THREE.MeshPhongMaterial({color: 0xff00ff, flatShading: true});
+
+    gltf.scene.traverse((child) => {
+        if (child.isMesh) {
+        child.material = material;
+        }
+    });
+
+    scene.add(gltf.scene);
+    }, undefined, (error) => {
+        console.error('An error happened:', error);
+    });
+}
 // const loader = new GLTFLoader();
 // const model = document.getElementById("model").innerText;
 
